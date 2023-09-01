b3e5d0e94ef48d3e2dda4176d24bcac785b1e8b1
Chigozie Mbama Mykel <92220456+mbamamykel77@users.noreply.github.com>
2023-09-01 20:51:25 +0100

    added logic to the dice funtion

--- index.js
+++ index.js
@@ -1,23 +1,79 @@
-let rollButton = document.querySelector(".roll")
-let dice_container = document.querySelector(".dice_cont")
-let dice = document.querySelectorAll(".item")
-
-// rollButton.style.backgroundColor = "red"
-// dice.style.backgroundColor = "red"
-
-// for (let i = 0; i < dice.length; i++){
-//     dice[i].style.backgroundColor = "red"
-//     console.log("are you working?")
-
-// }
+let rollButton = document.querySelector(".roll");
+let dice_container = document.querySelector(".dice_cont");
+let dice = Array.from(document.querySelectorAll(".item")); // Convert NodeList to an array
+let textBack = document.querySelector(".text_cont");
+let resetButton = document.querySelector(".resetButton")
 
 
-function roll (){
-    for (let i = 0; i < dice.length; i++){ 
-        let randomValue = Math.floor(Math.random() * 6) + 1; // Generates a random number from 1 to 6
-        dice[i].textContent = randomValue; // Update the content of the item
-        console.log("are you working?")
-    }
+
+let clickedDice = [];
+
+function resetGame() {
+  // Clear values on dice
+  dice.forEach((die) => {
+    die.textContent = "";
+    die.classList.remove("active");
+  });
+
+  // Re-enable the roll button
+  rollButton.disabled = false;
+
+  // Clear the clickedDice array
+  clickedDice = [];
 }
+resetButton.addEventListener("click", resetGame);
 
-rollButton.addEventListener("click", roll)
+
+
+function roll() {
+  rollButton.disabled = true;
+  clickedDice = [];
+
+  function checkAllDiceHaveSameValue() {
+    const firstValue = dice[0].textContent;
+    return dice.every((die) => die.textContent === firstValue);
+  }
+
+  // Roll dice continuously until they all have the same value and are clicked
+  function continuousRoll() {
+    if (!checkAllDiceHaveSameValue() || clickedDice.length < dice.length) {
+      for (let i = 0; i < dice.length; i++) {
+        if (!dice[i].classList.contains("active")) {
+          dice[i].classList.add("roll-animation");
+          let randomValue = Math.floor(Math.random() * 6) + 1;
+          dice[i].textContent = randomValue;
+          dice[i].classList.remove("roll-animation");
+          if (!dice[i].classList.contains("active")) {
+            dice[i].textContent = randomValue;
+          }
+        }
+      }
+      setTimeout(continuousRoll, 1000);
+    } else {
+      alert("Yay.... Congrats, You Win!!!");
+      resetGame();
+    }
+  }
+
+  continuousRoll();
+}
+rollButton.addEventListener("click", roll);
+
+
+
+
+function dicey() {
+  this.classList.toggle("active");
+
+  if (this.classList.contains("active")) {
+    clickedDice.push(this.textContent);
+  } else {
+    const index = clickedDice.indexOf(this.textContent);
+    if (index !== -1) {
+      clickedDice.splice(index, 1);
+    }
+  }
+}
+for (let i = 0; i < dice.length; i++) {
+  dice[i].addEventListener("click", dicey);
+}
